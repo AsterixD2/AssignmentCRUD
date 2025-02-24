d3203dc317c5464edb4ed7c43bb901efa2b76522
AsterixD2 <asterixd2@yahoo.com>
2025-02-24 15:28:16 +0700

    update git

--- src/app/_components/form-create.jsx
+++ src/app/_components/form-create.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useActionState } from "react";
 import { createActivityAction } from "../_actions/action-create";
+import { Button, Input, Select, SelectItem } from "@heroui/react";
 
 export const FormCreate = () => {
   const [state, formAction, pending] = useActionState(
@@ -14,15 +15,20 @@ export const FormCreate = () => {
         <h3 className="text-lg font-bold">Add New Transaction</h3>
         <p>Input title and amount of your transaction</p>
       </section>
-      <input name="title" placeholder="Input Title" />
-      <input name="amount" type="number" placeholder="Input Amount" />
-      <select name="category">
-        <option value="credit">Credit</option>
-        <option value="debit">Debit</option>
-      </select>
-      <button disabled={pending}>
-        {pending ? "Saving.." : "Save Transaction"}
-      </button>
+      <Input variant="bordered" name="title" placeholder="Input Title" />
+      <Input
+        variant="bordered"
+        name="amount"
+        type="number"
+        placeholder="Input Amount"
+      />
+      <Select name="category">
+        <SelectItem key="credit">Credit</SelectItem>
+        <SelectItem key="debit">Debit</SelectItem>
+      </Select>
+      <Button type="submit" color="primary" isLoading={pending}>
+        Save
+      </Button>
     </form>
   );
 };
