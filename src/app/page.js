453527ee61710495e35ef6a727a0cfe833505691
AsterixD2 <asterixd2@yahoo.com>
2025-02-24 15:30:23 +0700

    tambah judul

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { DataList } from "./_components/data-list";
 export default function Page() {
   return (
     <div className="max-w-3xl m-auto py-12 space-y-8">
+      <Div className="font-semibold tracking-tight">Kemana Uangku?</Div>
       <FormCreate />
       <DataList />
     </div>
